0d93b09326f851da189b4d44ceda64e4b7f02965
KayalvizhiSadayappan <70716152+KayalvizhiSadayappan@users.noreply.github.com>
2026-08-08 13:25:02 -0400

    Updating predicting global nitrate code
    The code is modified to calculate the statistics of 1000 modelled nitrate concentrations for each of 2895895 river segments.

--- scripts/05_predict_global_nitrate.R
+++ scripts/05_predict_global_nitrate.R
@@ -1,14 +1,14 @@
 # ---------------------------------------------------------------
-# Script: 04_predict_global_nitrate.R
+# Script: 05_predict_global_nitrate.R
 # Purpose: Use the model ensemble to predict nitrate concentrations. 
 #         We have provided catchment characteristics for a subset of 
 #         global river networks as example input
 # Author: Kayalvizhi Sadayappan
-# Date: 2026-01-26
-# Version: 1.0.0
+# Date: 2026-08-07
+# Version: 1.0.1
 #
 # Inputs:
-#   - Folder: data/processed/
+#   - Folder: data/raaw/
 #   - Files: BRT_input_global_subset.txt
 #   - Source: The example input data is in the repository 
 #             
@@ -17,16 +17,17 @@
 #   - Files: predicted_global_nitrate_subset.txt
 #
 # Dependencies:
-#   - R packages: readr, xgboost, caret
-# Runtime: The code ran on 1 CPU with 32 GB of memory and completed in approximately 1 minute
+#   - R packages: readr, xgboost, caret, matrixStats
+# Runtime: The code ran on 4 CPU with 32 GB of memory each and completed in approximately 5 hours
 #         for sample subset. 
 # ---------------------------------------------------------------
 library(caret)
 library(xgboost)
 library(readr)
+library(matrixStats)
 
 #select root folder (user specific)
-root_folder <- "/global-river-nitrate-model/"
+root_folder <- "/global-river-nitrate-model"
 
 #set working directory
 setwd(root_folder)
@@ -35,15 +36,13 @@ start_time <- Sys.time()
 
 #load input data
 load("models/trained_models.RData")
-stream_char_cons <- read_delim(file="data/processed/BRT_input_global_subset.txt",
+stream_char_cons <- read_delim(file="data/raw/BRT_input_data_global.txt",
                             delim = "\t", escape_double = FALSE,trim_ws = TRUE)
 model_runs <- 1000
 
 #allocate memory to save predicted model results
-stream_nit_df <- data.frame(matrix(NA,nrow=nrow(stream_char_cons),ncol=2+model_runs))
-colnames(stream_nit_df) <- c("MERIT","COMID",paste0("model_",seq(1,model_runs,1)))
-stream_nit_df$MERIT <- stream_char_cons$MERIT
-stream_nit_df$COMID <- stream_char_cons$COMID
+stream_nit_df <- stream_char_cons[,c("COMID")]
+stream_nit_df[paste0("model_", 1:model_runs)] <- NA_real_
 
 #predict results from all model runs
 for(i in 1:model_runs){
@@ -51,17 +50,31 @@ for(i in 1:model_runs){
   dglobal_nit <- xgb.DMatrix(data = data.matrix(sel_input))
   set.seed(i+model_runs)
   nit_pred=predict(model_ensemble[[i]],dglobal_nit)
-  stream_nit_df[,i+2]=10^nit_pred
+  stream_nit_df[paste0("model_", i)]=10^nit_pred
   print(i)
 }
 
+#extract values as matrix to reduce computation time for calcualting stastics over 1000 model runs
+model_values <- as.matrix(stream_nit_df[paste0("model_", 1:model_runs)])
+
 #get model ensemble averaged predicted nitrate concentrations and
 #their standard deviation as measure of uncertainty
-mean_nit_df=stream_nit_df[,1:2]
-mean_nit_df$mean_nit=apply(stream_nit_df[,(1:model_runs+2)],1,mean)
-mean_nit_df$sd_nit=apply(stream_nit_df[,(1:model_runs+2)],1,sd)
+stream_nit_df_summary <- data.frame(
+  COMID = stream_nit_df$COMID,
+  mean_nit = rowMeans2(model_values),
+  sd_nit = rowSds(model_values),
+  cv_nit = NA_real_,
+  p10_nit = rowQuantiles(model_values, probs = 0.10),
+  p50_nit = rowQuantiles(model_values, probs = 0.50),
+  p90_nit = rowQuantiles(model_values, probs = 0.90)
+)
+ 
+stream_nit_df_summary$cv_nit=stream_nit_df_summary$sd_nit*100/stream_nit_df_summary$mean_nit
 
-write.table(mean_nit_df,"results/predicted_global_nitrate_subset.txt",
+write.table(stream_nit_df_summary,"results/predicted_nitrate_concentrations_global.txt",
+            sep="\t",row.names=FALSE)
+
+write.table(stream_nit_df,"results/predicted_nitrate_concentrations_global_ensemble.txt",
             sep="\t",row.names=FALSE)
 
 run_time <- Sys.time()-start_time
